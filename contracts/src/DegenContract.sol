41daedce48d6b71ff5ec894f3192312cb2c69a5f
Lilly <reubenh14@tutanota.com>
2023-07-22 16:33:33 +0200

    contracts

--- contracts/src/DegenContract.sol
+++ contracts/src/DegenContract.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Router} from "gxm-contracts/core/interfaces/IRouter.sol";
+
 contract DegenContract {
-    address public constant ROUTER = 0x526f42EA12E167E36E3De747187f53b6989d121A; //arbitrum goerli testnet
+    address public constant POSITION_ROUTER =
+        0xb87a436B93fFE9D75c5cFA7bAcFff96430b09868; //arbitrum mainnet
+    address public constant ROUTER = 0x5F719c2F1095F7B9fc68a68e35B51194f4b6abe8;
 
     constructor() {}
+
+    function trade(address token, uint amount) {
+        IRouter(ROUTER).approvePlugin(POSITION_ROUTER);
+        ERC20(token).approve(ROUTER, amount);
+    }
 }
--- contracts/src/IDegen.sol
+++ contracts/src/IDegen.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.13;
 
 interface IDegen {
-
+    // 1. approve assets with GMX
+    //  2. execute trade with GMX
+    // amountIn * leverage
 }
